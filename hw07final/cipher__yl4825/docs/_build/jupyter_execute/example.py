5070a3fb1b63375cdeeecb7cf4094f0f246226f5
Liao Nicole <liaonicole@Liaos-MacBook-Pro.local>
2021-11-11 22:22:46 -0500

    html

---
+++ hw07final/cipher__yl4825/docs/_build/jupyter_execute/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Example usage
+# 
+# To use `cipher__yl4825` in a project:
+
+# In[1]:
+
+
+import cipher__yl4825
+
+print(cipher__yl4825.__version__)
+
