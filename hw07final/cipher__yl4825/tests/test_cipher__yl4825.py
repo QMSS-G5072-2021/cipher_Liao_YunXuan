c59ef20371973221cceb5b49f79d65de92ee5de2
Liao Nicole <liaonicole@Liaos-MacBook-Pro.local>
2021-11-11 22:24:59 -0500

    test

--- hw07final/cipher__yl4825/tests/test_cipher__yl4825.py
+++ hw07final/cipher__yl4825/tests/test_cipher__yl4825.py
@@ -1,1 +1,8 @@
 from cipher__yl4825 import cipher__yl4825
+
+import pytest
+
+def test_single():
+    actual = cipher__yl4825.cipher(text = "hi", shift = 1)
+    expected = "ij"
+    assert actual == expected, "Should give ij"
